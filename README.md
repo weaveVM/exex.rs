8c2392cc9848ee014be42f0b703da7214a7e2737
Max Knivets <knivets.m@gmail.com>
2022-07-27 11:37:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), pre-packaged with TailwindCSS.
+
+## How 2 install
+
+`npm install`
+
+## `How 2 run`
+
+`npm run start`
---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-cyan-500 to-blue-500">
+      <h1 className="text-3xl animate-bounce h-full font-bold text-white flex items-center justify-center">
+        <div className="animate-spin">
+          Hello World!
+        </div>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
